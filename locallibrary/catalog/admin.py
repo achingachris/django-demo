d5d28e2fa91f6fb2d893e5d520b6bc7107909f92
ChrisAchinga <achinga.chris@gmail.com>
2019-11-06 16:34:19 +0300

    take 2

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,40 +1,75 @@
 from django.contrib import admin
 
-from catalog.models import Author, Genre, Book, BookInstance
+# Register your models here.
 
-# admin.site.register(Book)
-# admin.site.register(Author)
+from .models import Author, Genre, Book, BookInstance, Language
+
+"""Minimal registration of Models.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
 admin.site.register(Genre)
-# admin.site.register(BookInstance)
+admin.site.register(Language)
+"""
 
-# Define the admin class
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
-
-# Register the Admin classes for Book using the decorator
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInline]
 
 
 class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
     model = BookInstance
 
 
-# Register the Admin classes for BookInstance using the decorator
-@admin.register(BookInstance) 
+class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
+@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
-    # inlines = [BooksInstanceInline]
